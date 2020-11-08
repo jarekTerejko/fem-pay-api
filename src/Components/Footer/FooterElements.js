2379572e8eab420d0d7417d5e61117261dd2da18
jarekTerejko <j.terejko@gmail.com>
2020-11-08 07:10:36 +0100

    add circle

--- src/Components/Footer/FooterElements.js
+++ src/Components/Footer/FooterElements.js
@@ -1,8 +1,22 @@
 import styled from "styled-components";
+import Circle from "../../images/shared/desktop/bg-pattern-circle.svg";
 
 export const FooterElement = styled.footer`
   background: var(--mirage-blue);
   position: relative;
+  overflow: hidden;
+  z-index: 1;
+
+  &::before {
+    content: "";
+    position: absolute;
+    width: 780px;
+    height: 780px;
+    background: url(${Circle});
+    top: -170px;
+    right: -430px;
+    z-index: -1;
+  }
 `;
 export const FooterElementSocialsWrapper = styled.ul`
   list-style: none;
