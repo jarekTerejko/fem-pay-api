8502483b2b55377b23813edcfe6fe27404e3c24d
jarekTerejko <j.terejko@gmail.com>
2020-11-16 08:25:35 +0100

    add Question and Footer components

--- src/Pages/Pricing.js
+++ src/Pages/Pricing.js
@@ -1,6 +1,8 @@
 import React from "react";
 import PricingMain from "../Components/PricingMain";
 import SiteHeader from "../Components/SiteHeader";
+import Question from "../Components/Question";
+import Footer from "../Components/Footer";
 
 const Pricing = () => {
   const title = "Pricing";
@@ -9,6 +11,8 @@ const Pricing = () => {
     <>
       <SiteHeader title={title} />
       <PricingMain />
+      <Question />
+      <Footer />
     </>
   );
 };
