c6e1e453da336ceceeedd55a5a26433fefea9339
jarekTerejko <j.terejko@gmail.com>
2020-11-23 06:59:07 +0100

    change hover state

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,22 +1,23 @@
 import styled from "styled-components";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 export const Button = styled(Link)`
-    padding: 1.6rem 2.8rem;
-    background: var(--dark-pink);
-    border: 1px solid  var(--dark-pink);
-    color: #fff;
-    font-family: inherit;
-    text-decoration: none;
-    font-size: 1.3rem;
-    font-weight: 500;
-    letter-spacing: .8px;
-    cursor: pointer;
-    border-radius: 30px;
-    transition: .3s;
+  padding: 1.6rem 2.8rem;
+  background: var(--dark-pink);
+  border: 1px solid var(--dark-pink);
+  color: #fff;
+  font-family: inherit;
+  text-decoration: none;
+  font-size: 1.3rem;
+  font-weight: 500;
+  letter-spacing: 0.8px;
+  cursor: pointer;
+  border-radius: 30px;
+  transition: 0.3s;
 
-    &:hover{
-        background: #fff;
-        color: var(--dark-pink);
-    }
-`
+  &:hover {
+    background: var(--charm-pink);
+    color: var(--link-water-white);
+    border-color: var(--charm-pink);
+  }
+`;
